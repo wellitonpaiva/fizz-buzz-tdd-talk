cfbde376ade850e85d887d868f0359f16fa1d3f8
Welliton Paiva <wellitonpaiva@gmail.com>
2019-04-30 21:43:10 -0300

    Initial commit

---
+++ src/test/java/com/thescientist/NativeFizzBuzzIT.java
@@ -0,0 +1,9 @@
+package com.thescientist;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeFizzBuzzIT extends FizzBuzzTest {
+
+    // Execute the same tests but in native mode.
+}
